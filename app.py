c1808b78baa3be9a6490b219ab38641e2de98643
REFLAXua <114022873+REFLAXua@users.noreply.github.com>
2024-06-24 17:48:43 +0300

    Create app.py
    This is main script, where we can conversate file from Markdown to HTML

---
+++ app.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import markdown
+
+def convert_markdown_to_html(input_path, output_path=None):
+    try:
+        html_content = markdown.markdown(markdown_content)
+    except Exception as e:
+        print(f"error: invalid markdown <{e}>", file=sys.stderr)
+        sys.exit(1)
+
+    if output_path:
+        try:
+            with open(output_path, 'w') as file:
+                file.write(html_content)
+        except Exception as e:
+            print(f"Error writing to file '{output_path}': {e}", file=sys.stderr)
+            sys.exit(1)
+    else:
+        print(html_content)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Using: ./app /path/to/markdown [--out /path/to/output.html]", file=sys.stderr)
+        sys.exit(1)
+
+    input_path = sys.argv[1]
+    output_path = None
+
+    if len(sys.argv) == 4 and sys.argv[2] == '--out':
+        output_path = sys.argv[3]
+
+    convert_markdown_to_html(input_path, output_path)
